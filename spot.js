e6967b8eb9361060e10f14e34d4a19bedd820a72
yyb1234-56 <66711058+yyb1234-56@users.noreply.github.com>
2022-03-08 22:21:54 +0800

    Add files via upload

---
+++ spot.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    var canvas = document.getElementById('canvas');
+
+    var engine = new Shape.Engine(canvas);
+    var promise = new Promise((resolve) => { resolve(); });
+    
+    document.getElementById('go').addEventListener('click', start);
+    
+    function start() {
+        document.getElementById('go').removeEventListener('click', start);
+        promise.then(() => engine.toText('Happy'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('Girls Day'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('To'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('YueYue'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('(-^_^-)'))
+        .then(() => engine.shake())
+        .then(() => engine.clear())
+        .then(() => document.getElementById('go').addEventListener('click', start));
+    }
+})();
